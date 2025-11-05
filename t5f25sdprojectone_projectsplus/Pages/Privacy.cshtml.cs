09f7444c94d494d432c83433846e16b85c5759d5
wnforne@gmail.com <mnforne@gmail.com>
2025-11-05 11:30:30 -0500

    add starter code

---
+++ t5f25sdprojectone_projectsplus/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace t5f25sdprojectone_projectsplus.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
