999238fe495576907a50720c67069b7bc1a500fa
cbugarin <cbugarin@my.centennialcollge.ca>
2025-11-14 13:00:32 -0500

    Implement admin user management UI (closes #22)

--- t5f25sdprojectone_projectsplus/obj/Debug/net8.0/t5f25sdprojectone_projectsplus.AssemblyInfo.cs
+++ t5f25sdprojectone_projectsplus/obj/Debug/net8.0/t5f25sdprojectone_projectsplus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d90a854a8539e6a1c2237b24f6b670ebe5b8b968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8538ec07c331b4b9463318482fee251941264dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("t5f25sdprojectone_projectsplus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
